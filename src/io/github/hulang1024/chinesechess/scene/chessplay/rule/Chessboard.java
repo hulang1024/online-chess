9b6c172e1353e67af735f80d2532c75a98fc1fde
hulang <1013644379@qq.com>
2020-10-21 15:25:07 +0800

    Add

---
+++ src/io/github/hulang1024/chinesechess/scene/chessplay/rule/Chessboard.java
@@ -0,0 +1,35 @@
+package io.github.hulang1024.chinesechess.scene.chessplay.rule;
+
+import io.github.hulang1024.chinesechess.scene.chessplay.rule.chess.AbstractChess;
+
+/**
+ * 棋盘
+ * @author Hu Lang
+ */
+public interface Chessboard {
+    int ROW_NUM = 10;
+    int COL_NUM = 9;
+
+    /**
+     * 指定位置是否为空位
+     * @param row
+     * @param col
+     * @return
+     */
+    boolean isEmpty(int row, int col);
+
+    /**
+     * 获取指定位置上棋子
+     * @param pos
+     * @return
+     */
+    AbstractChess chessAt(ChessPosition pos);
+
+    /**
+     * 获取指定位置上棋子
+     * @param row
+     * @param col
+     * @return
+     */
+    AbstractChess chessAt(int row, int col);
+}
