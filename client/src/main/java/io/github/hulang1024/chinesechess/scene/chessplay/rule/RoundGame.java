1370428dbc393cbd121efa338a68291b7f7241c5
hulang1024 <1013644379@qq.com>
2020-10-22 22:06:34 +0800

    DrawableChessboard改成Chessboard接口

--- client/src/main/java/io/github/hulang1024/chinesechess/scene/chessplay/rule/RoundGame.java
+++ client/src/main/java/io/github/hulang1024/chinesechess/scene/chessplay/rule/RoundGame.java
@@ -1,9 +1,7 @@
 package io.github.hulang1024.chinesechess.scene.chessplay.rule;
 
-import io.github.hulang1024.chinesechess.scene.chessplay.DrawableChessboard;
-
 /**
- * 游戏局
+ * 游戏对局
  * @author Hu Lang
  */
 public interface RoundGame {
@@ -11,7 +9,7 @@ public interface RoundGame {
      * 棋盘
      * @return
      */
-    DrawableChessboard getChessboard();
+    Chessboard getChessboard();
 
     /**
      * 查看指定方是否布局在棋盘顶部
