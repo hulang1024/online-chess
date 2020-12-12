5a5a6a655ec60baad9bda473efd99c30532ad7d9
hulang1024 <1013644379@qq.com>
2020-12-12 22:52:04 +0800

    .

---
+++ server/src/main/java/io/github/hulang1024/chinesechess/userstats/SearchRankingParam.java
@@ -0,0 +1,15 @@
+package io.github.hulang1024.chinesechess.user;
+
+import lombok.Data;
+
+import javax.validation.constraints.NotNull;
+
+@Data
+public class SearchRankingParam {
+    /**
+     * 1=根据综合表现
+     * 2=根据局数
+     */
+    @NotNull
+    private Integer rankingBy;
+}
