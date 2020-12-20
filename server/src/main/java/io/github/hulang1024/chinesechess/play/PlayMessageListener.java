c7fa027545a5df7f8057bee3e850bf933a2981f0
hulang1024 <1013644379@qq.com>
2020-12-20 17:20:36 +0800

    Fix(Play): 掉线继续用户选择否房间删除事件

--- server/src/main/java/io/github/hulang1024/chinesechess/play/PlayMessageListener.java
+++ server/src/main/java/io/github/hulang1024/chinesechess/play/PlayMessageListener.java
@@ -236,7 +236,7 @@ public class PlayMessageListener extends AbstractMessageListener {
             GameContinueResponseServerMsg serverMsg = new GameContinueResponseServerMsg();
             serverMsg.setOk(clientMsg.isOk());
             serverMsg.setUid(user.getId());
-            roomManager.broadcast(joinedRoom, serverMsg, user);
+            roomManager.broadcast(joinedRoom, serverMsg);
         }
     }
 
