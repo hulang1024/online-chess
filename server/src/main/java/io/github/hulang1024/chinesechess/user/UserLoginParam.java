9597e0eb4efddd7f5e14d7521de02e57f9879ec2
hulang1024 <1013644379@qq.com>
2020-12-29 18:43:43 +0800

    Fix(User):登录username可空

--- server/src/main/java/io/github/hulang1024/chinesechess/user/UserLoginParam.java
+++ server/src/main/java/io/github/hulang1024/chinesechess/user/UserLoginParam.java
@@ -2,11 +2,8 @@ package io.github.hulang1024.chinesechess.user;
 
 import lombok.Data;
 
-import javax.validation.constraints.NotBlank;
-
 @Data
 public class UserLoginParam {
-    @NotBlank
     private String username;
 
     private String password;
