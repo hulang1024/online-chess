5e062b03d5083f9d8b74fca6aca3aaad9f85c57d
hulang1024 <1013644379@qq.com>
2021-01-15 13:44:20 +0800

    Update(Play):设置未启用游戏声音时跳过音频加载

--- client/src/pages/play/GameAudio.ts
+++ client/src/pages/play/GameAudio.ts
@@ -3,13 +3,14 @@ import { ConfigItem } from "src/config/ConfigManager";
 
 export default class GameAudio {
   public static play(name: string) {
+    if (!configManager.get(ConfigItem.audioGameEnabled)) {
+      return;
+    }
     const audio = audioManager.samples.get(`game/${name}`);
     if (!audio.src) {
       audio.src = `/chinesechess/themes/default/audio/${name}.wav`;
     }
-    if (configManager.get(ConfigItem.audioGameEnabled)) {
-      // eslint-disable-next-line
-      audio.play();
-    }
+    // eslint-disable-next-line
+    audio.play();
   }
 }
