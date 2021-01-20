eaa1b833619cf43d09fa5ee5e0347ec8635e13a3
hulang1024 <1013644379@qq.com>
2021-01-20 15:06:34 +0800

    Update: 游戏音频预加载

--- client/src/audio/SampleStore.ts
+++ client/src/audio/SampleStore.ts
@@ -1,6 +1,12 @@
 export default class SampleStore {
   private readonly sampleCache: { [name: string]: HTMLAudioElement } = {};
 
+  constructor() {
+    setTimeout(() => {
+      this.loadAll();
+    }, 0);
+  }
+
   public get(name: string): HTMLAudioElement {
     let audio = this.sampleCache[name];
     if (audio) {
@@ -8,6 +14,9 @@ export default class SampleStore {
     }
 
     audio = new Audio();
+    audio.src = `/chinesechess/themes/default/audio/${name}.wav`;
+    audio.load();
+
     this.sampleCache[name] = audio;
 
     return audio;
@@ -17,5 +26,12 @@ export default class SampleStore {
     Object.keys(this.sampleCache).forEach((name) => {
       this.sampleCache[name].volume = volume;
     });
+  }
+
+  private loadAll() {
+    for (let count = 1; count <= 10; count++) {
+      this.get(`count/${count}`);
+    }
+    this.get('click');
   }
 }
