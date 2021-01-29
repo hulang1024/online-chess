78f2ac62ef9f21182284a6ed6ca545116756e5fe
hulang1024 <1013644379@qq.com>
2021-01-29 12:49:39 +0800

    Update(User): 游客用户id以负100开始，以区分开以后可能会增加的系统用户

--- server/src/main/java/io/github/hulang1024/chinesechess/user/GuestUser.java
+++ server/src/main/java/io/github/hulang1024/chinesechess/user/GuestUser.java
@@ -1,7 +1,7 @@
 package io.github.hulang1024.chinesechess.user;
 
 public class GuestUser extends User {
-    private static long id = -1;
+    private static long id = -101;
     public GuestUser() {
         this.setId(id--);
         this.setNickname("Guest");
