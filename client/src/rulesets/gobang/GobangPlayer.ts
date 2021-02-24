fb6a6c275edf5958c620a6a1c9a39e07de667f06
hulang1024 <1013644379@qq.com>
2021-02-24 12:26:19 +0800

    Update(Gobang): 修改设置窗口模态模式和文案

--- client/src/rulesets/gobang/GobangPlayer.ts
+++ client/src/rulesets/gobang/GobangPlayer.ts
@@ -5,16 +5,15 @@ export default class GobangPlayer extends RulesetPlayer {
   public openSettings() {
     const userPlayInput = this.userPlayInput as GobangUserPlayInput;
     this.context.$q.dialog({
-      title: '选择输入模式',
+      title: '选择落子模式',
       options: {
         type: 'radio',
         model: userPlayInput.mode.toString(),
         items: [
           { label: '点击一次确定落子位置', value: '1' },
-          { label: '点击两次相同位置才确定落子位置', value: '2' },
+          { label: '连续点击两次相同位置确定落子位置', value: '2' },
         ],
       },
-      persistent: true,
       ok: {
         label: '确定',
         color: 'primary',
