e852a663eee6c5f7b928ad46813872bca17e4ec5
hulang1024 <1013644379@qq.com>
2021-02-24 23:15:25 +0800

    Clean Code

--- client/src/rulesets/gobang/ui/MouseChessTarget.ts
+++ client/src/rulesets/gobang/ui/MouseChessTarget.ts
@@ -1,4 +1,5 @@
 import ChessHost from 'src/rulesets/chess_host';
+import ChessPos from 'src/rulesets/gobang/ChessPos';
 import GobangDrawableChessboard from './GobangDrawableChessboard';
 import { calcChessDisplayPos, calcChessSize } from './DrawableChess';
 import './chess.scss';
@@ -24,14 +25,14 @@ export default class MouseChessTarget {
     classList.add(chess == ChessHost.FIRST ? 'black' : 'white');
   }
 
-  public show(row: number, col: number) {
+  public show(pos: ChessPos) {
     const { el } = this;
 
     el.classList.add('show');
     const size = calcChessSize(this.chessboard.sizes);
     el.style.width = `${size}px`;
     el.style.height = `${size}px`;
-    const displayPos = calcChessDisplayPos(row, col, this.chessboard.sizes);
+    const displayPos = calcChessDisplayPos(pos.row, pos.col, this.chessboard.sizes);
     el.style.top = `${displayPos.top}px`;
     el.style.left = `${displayPos.left}px`;
   }
