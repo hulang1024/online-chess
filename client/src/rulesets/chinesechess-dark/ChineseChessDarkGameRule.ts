83ab4df13afa2e3c8e9d9d3fdd450461e84324da
hulang1024 <1013644379@qq.com>
2021-05-03 14:14:27 +0800

    Fix(ChineseChessDark): 修正揭棋中暗士可以离开九宫bug

--- client/src/rulesets/chinesechess-dark/ChineseChessDarkGameRule.ts
+++ client/src/rulesets/chinesechess-dark/ChineseChessDarkGameRule.ts
@@ -6,6 +6,7 @@ import ChessG from '../chinesechess/rule/ChessG';
 import ChessK from '../chinesechess/rule/ChessK';
 import ChessM from '../chinesechess/rule/ChessM';
 import ChessPos from '../chinesechess/rule/ChessPos';
+import { isInKingHome } from '../chinesechess/rule/move_rules';
 import ChineseChessDarkGameSettings from './ChineseChessDarkGameSettings';
 import { queryMoveRuleByOriginPos } from './rules';
 
@@ -30,7 +31,9 @@ export default class ChineseChessDarkGameRule extends ChineseChessGameRule {
     if (moveRule.is(ChessM)) {
       (moveRule as ChessM).canCrossBoundary = true;
     } else if (moveRule.is(ChessG)) {
-      (moveRule as ChessG).canGoOutside = true;
+      if (!(moveRule as ChessG).canGoOutside && !isInKingHome(moveRule, moveRule.getPos(), this)) {
+        (moveRule as ChessG).canGoOutside = true;
+      }
     } else if (this.gameSettings.fullRandom && moveRule.is(ChessK)) {
       (moveRule as ChessK).canGoOutside = true;
     }
